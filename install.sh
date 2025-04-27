c1d0bbb99b20d470e48b79f2541233c875e65a41
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2025-04-27 11:10:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 #set -x
 
+CURRENT=$(dirname $0)
+
 function install()
 {
   RC_FILE=$HOME/$1
   if [ ! -e $RC_FILE ]
   then
-    ln -s $PWD/$1 $RC_FILE
+    ln -s $CURRENT/$1 $RC_FILE
   else
     echo "Skip $1"
   fi
